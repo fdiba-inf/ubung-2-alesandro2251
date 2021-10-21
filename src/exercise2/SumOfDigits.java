cd25d7bb94a075e96695e42e410fedb6128be075
alesandro2251 <>
2021-10-21 08:18:06 +0000

    Files updated

---
+++ src/exercise2/SumOfDigits.java
@@ -0,0 +1,20 @@
+package exercise2;
+import java.util.Scanner;
+
+public class SumOfDigits {
+   public static void main(String[] args){
+     
+     Scanner input = new Scanner(System.in);
+     int a = input.nextInt();
+     int b = input.nextInt();
+     int d = input.nextInt();
+     int c = input.nextInt();
+     int sum = a + b + c + d;
+     System.out.println("Sum of digits:"+ sum);
+     
+
+     
+   }
+   
+   
+}
